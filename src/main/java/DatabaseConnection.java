67b399a08784305117428dae17a2c45f68ee06c8
TheHolyPit GuardianAccess <yannickbou@gmx.de>
2025-12-20 17:14:43 +0100

    Hinzufügen von mehreren Daten.

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -1,5 +1,5 @@
 import java.sql.*;
-
+// In diesem Java Code wird die Verbindung zwischen Java und der SQL hergestellt, bzw. sollte so sein, ich arbeite noch dran.
 public class DatabaseConnection {
     private static final String URL = "";
     private static final String User = "root";
